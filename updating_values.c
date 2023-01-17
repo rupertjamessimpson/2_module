9441c50cd3ed88c3e3fe5ea6f546f2cf39d56d95
Rupert Simpson <rupertjsimpson@login-students.khoury.northeastern.edu>
2023-01-17 16:23:55 -0500

    Added pointer practice and practice on updating by memory location

---
+++ updating_values.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void update(int argument){
+    argument = 10;
+}
+
+int main(){
+    int value = 5;
+    int* p_value = &value;
+    printf("Value is %d\n", value);
+    printf("The address of value is: %p\n", p_value);
+    printf("The value at p_value is: %d\n\n", *p_value);
+
+    // used to be a 5, but now it is 15
+    *p_value = 15;
+
+    printf("The updated value at p_value is: %d\n", *p_value);
+    printf("Value updated is: %d\n", value);
+    return 0;
+}
