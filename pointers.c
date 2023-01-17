f0c6696e2ae6e2abc0e869a261699ec71d6d2df8
Rupert Simpson <rupertjsimpson@login-students.khoury.northeastern.edu>
2023-01-17 16:04:24 -0500

    Created a file practicing pointers in c

---
+++ pointers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void update(int argument){
+    argument = 10;
+}
+
+int main(){
+    int value = 5;
+    int* p_value = &value;
+    printf("Value is %d\n", value);
+    printf("The address of value is: %p\n", p_value);
+    update(value);
+    printf("Value updated is: %d\n", value);
+    return 0;
+}
