9dfb3199733eefafcdb0b8f36bbf3651665d3666
Rupert Simpson <rupertjsimpson@login-students.khoury.northeastern.edu>
2023-01-17 14:32:29 -0500

    created new folder for c practice files

---
+++ types.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//This is the entry point to my program.
+int main(){
+    int accountId = 5;
+    double accountBalance = 100.53;
+    char accountType = 'C';
+
+	printf("AccountID is: %d\n", accountId);
+    printf("AccountBL is: %f\n", accountBalance);
+    printf("AccountTY is: %c\n", accountType);
+
+    printf("size of(int): %d\n", sizeof(int));
+    printf("size of(double): %d\n", sizeof(double));
+    printf("size of(char): %d\n", sizeof(char));
+	return 0;
+}
